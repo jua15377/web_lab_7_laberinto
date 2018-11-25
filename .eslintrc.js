d74f927646bf81ae150af005b1489f927902b544
jua15377 <jua15377@uvg.edu.gt>
2018-11-24 20:19:16 -0600

    linter install

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+const OFF = 0, WARN = 1, ERROR = 2;
+module.exports = {
+  "extends": ["airbnb"],
+  "parser": "babel-eslint",
+  "parserOptions": {
+    "ecmaVersion": 9,
+    "sourceType": "module",
+    "allowImportExportEverywhere": false,
+    "codeFrame": false,
+    "ecmaFeatures": {
+      globalReturn: true,
+      impliedStrict: true,
+      jsx: true,
+      arrowFunction: true
+     }
+  },
+  "plugins": ["react"],
+  "env": {
+    "es6": true,
+    "browser": true
+  },
+ "rules":{
+    "react/jsx-filename-extension": [WARN, { "extensions": [".js", ".jsx"] }],
+    "react/jsx-indent": [OFF],
+    "linebreak-style" : ["error", "windows"],
+    "semi": ["error", "never"],
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,81 +1,104 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import axios from 'axios';
+import axios from 'axios'
 import './style.css'
 
-class Maze extends React.Component{
 
-	constructor(props) {
-		super(props);
-	
-		this.state = {
-		  mazeContent: [],
+class Maze extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      mazeContent: [],
 		  isLoading: false,
 		  error: null,
 		  w: this.props.w,
-		  h: this.props.h
-		};
+		  h: this.props.h,
+		  }
 	  }
-	  getMaze(){
-		const baseUrl = 'http://34.210.35.174:3001/'
-		let newUrl =  baseUrl + '?type=json&w='+ this.state.w +'&h=' + this.state.h
-		return newUrl
-	}
-	
-	  componentDidMount() {
-		this.setState({ isLoading: true });
-		let api = this.getMaze()
-		axios.get(api)
-		  .then(result => this.setState({
-			mazeContent: result.data,
-			isLoading: false
-		  }))
-		  .catch(error => this.setState({
-			error,
-			isLoading: false
-		  }));
-	  }	
 
-	render(){
-		console.log(this.state.mazeContent)
-		return(
+	  getMaze() {
+		  const baseUrl = 'http://34.210.35.174:3001/'
+		  const newUrl = `${baseUrl}?type=json&w=${this.state.w}&h=${this.state.h}`
+		  return newUrl
+  }
+
+	  componentDidMount() {
+		  this.setState({ isLoading: true })
+		  const api = this.getMaze()
+		  axios.get(api)
+		    .then(result => this.setState({
+			    mazeContent: result.data,
+			    isLoading: false,
+		    }))
+		    .catch(error => this.setState({
+			    error,
+			    isLoading: false,
+		     }))
+		}
+
+
+  render() {
+    // console.log(this.state.mazeContent)
+    return (
 			<div>
 				<h1>HelloW</h1>
-				<div className="Maze_4">{
-				this.state.mazeContent.map((elemnt)=>{
-					{console.log("line",elemnt)}
-					return(
-					elemnt.map((piece)=>{
-						let iswall = false
-						{console.log("pieces",piece)}
-						if(piece === "+"){
-							return <div className="intersection"></div>
-						}
-						else if(piece === "|"){
-							return <div className="verticalWall"></div>
-						}
-						else if(piece === "-"){
-							return <div className="horizontalWall"></div>
-						}
-						else if(piece === "p"){
-							return <div className="player_up"></div>
-						}
-						else if(piece === "g"){
-							return <div className="goal"></div>
-						}
-						else{
-							return <div className="walk"></div>
-						}
-					}))
+				<div className="Maze_4">
+{
+				this.state.mazeContent.map((elemnt, key) => {
+				  { console.log('line', elemnt) }
+				  return (
+				    elemnt.map((piece, key2) => {
+				      const iswall = false
+				      { console.log('pieces', piece) }
+				      if (piece === '+') {
+				        return <div className="intersection" />
+				      } if (piece === '|') {
+				        return <div className="verticalWall" />
+				      } if (piece === '-') {
+				        return <div className="horizontalWall" />
+				      } if (piece === 'p') {
+				        return <Player x={key2} y={key}/>
+				      } if (piece === 'g') {
+				        return <div className="goal" />
+				      }
+				        return <div className="walk" />
+				    }))
 				})
-			}
-			</div>
-		</div>
-		)
-	}
-	
+			  }
+    </div>
+   </div>
+    )
+  }
 }
 
+class Player extends React.Component{
+	constructor(props) {
+    super(props)
+    this.state = {
+			x: this.props.x,
+			y: this.props.y
+		  }
+		}
+		keyboardInput (e) {
+			console.log(e)
+			let code = e.keyCode ? e.keyCode : e.which;
+			// Change X and Y values
+			if (code === 38) { //up key
+					conosole.log("up");
+			} else if (code === 40) { //down key
+					conosole.log("down")
+			} else if (code === 37) { // left key
+					console.log("lef")
+			} else if (code === 39) { // right key
+				  console.log("rigth")
+			}
+	}
+	
+	render() {
+		return(
+			<div className="player_up" onKeyDown={this.keyboardInput} />
+		)
+	}
+}
 
-
-ReactDOM.render( <Maze w={4} h={4}/>, document.getElementById('index'))
+ReactDOM.render(<Maze w={4} h={4} />, document.getElementById('index'))
